5061518525655e6c02b197b2435688323ed0d564
Chris Impey <chris@bigwednesday.io>
2015-11-24 15:52:56 +0000

    retrieve postcode from google datastore

--- server/postcodes.js
+++ server/postcodes.js
@@ -1,17 +1,45 @@
 'use strict';
 
+const Boom = require('boom');
 const Joi = require('joi');
 
 const postCodeSchema = Joi.object({
-  postcode: Joi.string().description('Postcode')
+  postcode: Joi.string().required().description('Postcode'),
+  place: Joi.string().allow(null).description('Place'),
+  district: Joi.string().allow(null).description('District'),
+  county: Joi.string().allow(null).description('County'),
+  region: Joi.string().description('Region'),
+  country: Joi.string().description('Country')
 }).meta({className: 'PostcodeData'});
+
+const dataset = require('../lib/dataset');
 
 exports.register = (server, options, next) => {
   server.route({
     method: 'GET',
     path: '/postcodes/{postcode}',
     handler: (request, reply) => {
-      reply({postcode: request.params.postcode});
+      dataset.get(dataset.key(['Postcode', request.params.postcode.toUpperCase()]), (err, entity) => {
+        if (err) {
+          console.error(err);
+          return reply(err);
+        }
+
+        if (!entity) {
+          return reply(Boom.notFound());
+        }
+
+        const resource = {
+          postcode: entity.data.postcode,
+          place: entity.data.place.length ? entity.data.place : null,
+          district: entity.data.district.length ? entity.data.district : null,
+          county: entity.data.county.length ? entity.data.county : null,
+          region: entity.data.region,
+          country: entity.data.country
+        };
+
+        reply(resource);
+      });
     },
     config: {
       tags: ['api'],
--- spec/postcodes_{id}.js
+++ spec/postcodes_{id}.js
@@ -2,28 +2,85 @@
 
 const expect = require('chai').expect;
 
+const dataset = require('../lib/dataset');
 const specRequest = require('./spec_request');
+
+const entities = [
+  {key: dataset.key(['Postcode', 'HS12AA']), data: {postcode: 'HS12AA', place: 'Steòrnabhagh / Stornoway', district: '', county: 'Na h-Eileanan an Iar', region: 'Scotland', country: 'Scotland'}},
+  {key: dataset.key(['Postcode', 'DL104DE']), data: {postcode: 'DL104DE', place: 'Richmond', district: 'Richmondshire', county: 'North Yorkshire', region: 'Yorkshire and the Humber', country: 'England'}},
+  {key: dataset.key(['Postcode', 'L314EU']), data: {postcode: 'L314EU', place: '', district: 'West Lancashire', county: 'Lancashire', region: 'North West', country: 'England'}},
+  {key: dataset.key(['Postcode', 'CA101AA']), data: {postcode: 'CA101AA', place: 'Lazonby', district: 'Eden', county: 'Cumbria', region: 'North West', country: 'England'}}
+];
 
 describe('/postcodes/{id}', () => {
   describe('get', () => {
-    let getResponse;
+    let getResponses;
 
-    before(() => specRequest({url: '/postcodes/hs12aa', method: 'GET'}).then(response => {
-      getResponse = response;
-    }));
+    before(() =>
+      new Promise((resolve, reject) => {
+        dataset.save(entities, err => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve();
+        });
+      })
+      .then(() => Promise.all([
+        specRequest({url: '/postcodes/hs12aa', method: 'GET'}),
+        specRequest({url: '/postcodes/DL104DE', method: 'GET'}),
+        specRequest({url: '/postcodes/l314eu', method: 'GET'}),
+        specRequest({url: '/postcodes/CA101AA', method: 'GET'})
+      ]))
+      .then(responses => {
+        getResponses = responses;
+      })
+    );
 
     it('returns http 200', () => {
-      expect(getResponse.statusCode).to.equal(200);
+      getResponses.forEach(response => expect(response.statusCode).to.equal(200));
     });
 
     it('returns postcode resource', () => {
-      expect(getResponse.result).to.deep.equal({
+      expect(getResponses[0].result).to.deep.equal({
         postcode: 'HS12AA',
         place: 'Steòrnabhagh / Stornoway',
         district: null,
+        county: 'Na h-Eileanan an Iar',
         region: 'Scotland',
         country: 'Scotland'
       });
+
+      expect(getResponses[1].result).to.deep.equal({
+        postcode: 'DL104DE',
+        place: 'Richmond',
+        district: 'Richmondshire',
+        county: 'North Yorkshire',
+        region: 'Yorkshire and the Humber',
+        country: 'England'
+      });
+
+      expect(getResponses[2].result).to.deep.equal({
+        postcode: 'L314EU',
+        place: null,
+        district: 'West Lancashire',
+        county: 'Lancashire',
+        region: 'North West',
+        country: 'England'
+      });
+
+      expect(getResponses[3].result).to.deep.equal({
+        postcode: 'CA101AA',
+        place: 'Lazonby',
+        district: 'Eden',
+        county: 'Cumbria',
+        region: 'North West',
+        country: 'England'
+      });
+    });
+
+    it('returns http 404 for unknown postcodes', () => {
+      specRequest({url: '/postcodes/ab123de', method: 'GET'}).then(response => expect(response.statusCode).to.equal(404));
     });
   });
 });
